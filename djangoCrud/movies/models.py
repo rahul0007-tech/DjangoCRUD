2849c4b0d6b16c08b38d3d85452a820edcf6f20d
rahul0007-tech <rahultech730@gmail.com>
2024-09-09 12:18:51 +0545

    created a model for movie

---
+++ djangoCrud/movies/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+
+class Movie(models.Model):
+    image = models.ImageField(upload_to='movies/')
+    title = models.CharField(max_length=20)
+    director = models.CharField(max_length=20)
+    genre = models.CharField(max_length=20)
+    ratings = models.IntegerField()
+    description = models.CharField(max_length=100)
+
+    def __str__ (self):
+        return self.title
