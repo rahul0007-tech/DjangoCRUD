c237ce814efd88a7a303671f23fea37f951143e8
rahul0007-tech <rahultech730@gmail.com>
2024-09-09 12:29:56 +0545

    creating super user and listing movie details in admin pannel

--- djangoCrud/movies/admin.py
+++ djangoCrud/movies/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from . models import Movie
 
 # Register your models here.
+
+class MovieAdmin(admin.ModelAdmin):
+    list_display=('title','director', 'genre', 'ratings')
+
+
+admin.site.register(Movie, MovieAdmin)
