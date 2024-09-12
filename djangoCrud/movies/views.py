1e2f80141f45f91a8c96d4fe1f75fb52ae9b4100
rahul0007-tech <rahultech730@gmail.com>
2024-09-12 08:37:21 +0545

    written a function to add movie

--- djangoCrud/movies/views.py
+++ djangoCrud/movies/views.py
@@ -1,6 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from . models import Movie
 
 # Create your views here.
 def addMovie(request):
-    pass
+    if request.method == "POST":
+        image = request.POST.get('image')
+        title = request.POST.get('title')
+        director = request.POST.get('director')
+        genre =request.POST.get('genre')
+        ratings = request.POST.get('ratings')
+        description= request.POST.get('description')
+
+        movie = Movie(image=image, title=title, director=director, genre=genre, ratings=ratings, description=description)
+        movie.save()
+        return redirect('listMovie')
+    
+    return render(request, 'addMovie.html')
+
 
