e088d9fcfb183244d76318b2db092a8bd923bfbb
rahul0007-tech <rahultech730@gmail.com>
2024-09-12 09:29:38 +0545

    handled the form part of adding a movie

--- djangoCrud/movies/urls.py
+++ djangoCrud/movies/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('addMovie/', views.addMovie),
+    path('addMovie/', views.addMovie, name='addMovie'),
 ]
--- djangoCrud/movies/views.py
+++ djangoCrud/movies/views.py
@@ -4,7 +4,7 @@ from . models import Movie
 # Create your views here.
 def addMovie(request):
     if request.method == "POST":
-        image = request.POST.get('image')
+        image = request.FILES.get('image')
         title = request.POST.get('title')
         director = request.POST.get('director')
         genre =request.POST.get('genre')
@@ -13,7 +13,7 @@ def addMovie(request):
 
         movie = Movie(image=image, title=title, director=director, genre=genre, ratings=ratings, description=description)
         movie.save()
-        return redirect('listMovie')
+        # return redirect('listMovie')
     
     return render(request, 'addMovie.html')
 
