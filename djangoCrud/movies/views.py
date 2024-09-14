296a7cb10b952290bf32f63dd128971107bab25a
rahul0007-tech <rahultech730@gmail.com>
2024-09-14 11:47:38 +0545

    Created and implemented django forms

--- djangoCrud/movies/views.py
+++ djangoCrud/movies/views.py
@@ -1,23 +1,34 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from . models import Movie
+from .forms import MovieForm
 
 # Create your views here.
+# def addMovie(request):
+#     if request.method == "POST":
+#         image = request.FILES.get('image')
+#         title = request.POST.get('title')
+#         director = request.POST.get('director')
+#         genre =request.POST.get('genre')
+#         ratings = request.POST.get('ratings')
+#         releaseDate = request.POST.get('releaseDate')
+#         description= request.POST.get('description')
+
+#         movie = Movie(image=image, title=title, director=director, genre=genre, ratings=ratings, releaseDate=releaseDate, description=description)
+#         movie.save()
+#         return redirect('listMovie')
+    
+#     return render(request, 'addMovie.html')
+
 def addMovie(request):
     if request.method == "POST":
-        image = request.FILES.get('image')
-        title = request.POST.get('title')
-        director = request.POST.get('director')
-        genre =request.POST.get('genre')
-        ratings = request.POST.get('ratings')
-        releaseDate = request.POST.get('releaseDate')
-        description= request.POST.get('description')
-
-        movie = Movie(image=image, title=title, director=director, genre=genre, ratings=ratings, releaseDate=releaseDate, description=description)
-        movie.save()
+        form = MovieForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
         return redirect('listMovie')
+    else:
+        form=MovieForm()
     
-    return render(request, 'addMovie.html')
-
+    return render(request, 'addMovie.html', {"form":form})
 
 def listMovie(request):
     movies = Movie.objects.all()
